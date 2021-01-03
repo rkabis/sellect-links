3f88e7e6c5dd6719ccad2fb2522cace1d2dd6700
Ryan Abis <ryan.abis@gmail.com>
2021-01-03 12:05:26 +0800

    change business photo

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,11 +1,16 @@
 import React, { ReactElement } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto'
 
 const useStyles = makeStyles(() => ({
   photo: {
     height: 150,
     width: 150
+  },
+  genericPhoto: {
+    height: 100,
+    width: 100
   }
 }))
 
@@ -22,7 +27,7 @@ const ImageViewer = (props: Props): ReactElement => {
       {
         (url && url !== '')
           ? <img src={url} className={classes.photo} />
-          : <img src={'./businessPhotoPlaceholder.png'} className={classes.photo} />
+          : <AddAPhotoIcon className={classes.genericPhoto} />
       }
     </React.Fragment>
   )
